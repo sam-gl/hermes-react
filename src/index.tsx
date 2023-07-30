817bed260418c646ba47957201d6548b75ab5e96
Sam <91395755+sam-gl@users.noreply.github.com>
2023-07-30 13:47:51 +0100

    Add props to customise text and endpoint. Make request when form is submitted.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { ChangeEvent, ReactElement, SyntheticEvent, useState } from "react";
 import styles from "./styles.module.css";
 
-const Hermes = () => {
+interface HermesInterface {
+  title: string,
+  subtitle: string,
+  endpoint: string
+};
+
+const Hermes = (props: HermesInterface): ReactElement  => {
+  const [ email, setEmail ] = useState('');
+  const { title, subtitle, endpoint } = props;
+
+  const submitForm = async (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email })
+    });
+
+    const parsedResponse = await response.json();
+
+    console.log(parsedResponse);
+    return parsedResponse;
+  }
+
+  const handleForm = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  }
+
   return (
     <div className={styles.hermes}>
-      <div className={styles.hermesTitle}>Stay updated with a weekly update newsletter</div>
-      <div className={styles.hermesSubtitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis blanditiis corporis qui eligendi, odit harum obcaecati laudantium eos quas excepturi.</div>
-      <form className={styles.hermesForm}>
-        <input className={styles.hermesInput} type="email" placeholder="youremail@example.com" />
+      <div className={styles.hermesTitle}>{title}</div>
+      <div className={styles.hermesSubtitle}>{subtitle}</div>
+      <form className={styles.hermesForm} onSubmit={submitForm}>
+        <input className={styles.hermesInput} type="email" placeholder="youremail@example.com" onChange={handleForm} value={email} />
         <button className={styles.hermesButton}>Join</button>
       </form>
     </div>
